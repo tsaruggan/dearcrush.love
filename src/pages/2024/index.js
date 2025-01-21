9efac5d8e7a4249c1260244e4a0f0100b4a12874
tammmyz <tammy.zeng908@gmail.com>
2025-01-20 20:44:30 -0800

    new year new me

--- src/pages/2024/index.js
+++ src/pages/2024/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/react-in-jsx-scope */
 import Image from 'next/image'
 import styles from "@/styles/Home.module.css";
 
---
+++ src/pages/index.js
@@ -0,0 +1,61 @@
+/* eslint-disable react/react-in-jsx-scope */
+import Image from 'next/image'
+import styles from "@/styles/Home.module.css";
+
+import Meta from '../components/Meta';
+
+import beforeGif from '../../assets/2025/placeholder-bee.png'
+import afterGif from '../../assets/2025/placeholder-bee.png'
+
+import { useState } from "react";
+
+const messages = [
+  "you're as sweet as honey",
+  "you're my queen bee",
+  "ooof...\nthat stings",
+  "you are bee-you-tiful"
+];
+
+export default function Home() {
+    const [yesPressed, setYesCircled] = useState(false);
+
+    // set conditions for circling yes or no
+    // if yes circled, continue to after gif
+    // if no is almost circled, move position of no
+
+    return (
+        <>
+          <Meta
+            title="send this to your crush 💌"
+            description="digital valentine's day card"
+            image="social-2024.jpeg"
+          />
+          <main className={`${styles.main}`}>
+          { yesPressed ? (
+          <div className={`flex items-center ${styles.buttonsContainer}`}>
+            <Image
+              src={afterGif}
+              alt="yipee bee"
+              className={styles.valentineGif}
+            />
+          </div>
+          ) : (
+          <div className={`flex items-center ${styles.buttonsContainer}`}>
+            <Image
+              src={beforeGif}
+              alt="messenger bee coming up to you and asking if you would bee my valentine"
+              className={styles.valentineGif}
+            />
+
+            <span className={styles.message}>{"will you bee my valentine?"}</span>
+            
+            <div className={styles.buttonRow}>
+              yes
+              no
+            </div>
+          </div>
+          )}
+      </main>
+        </>
+      );
+}
