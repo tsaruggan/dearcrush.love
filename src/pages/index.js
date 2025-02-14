19b5c796167c81d49bef6f32a1b413a094bf7767
tsaruggan <tsaruggan@gmail.com>
2025-02-14 01:33:42 -0800

    rerouted

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,74 @@
+/* eslint-disable react/jsx-no-undef */
 /* eslint-disable react/react-in-jsx-scope */
+import Image from 'next/image'
 import styles from "@/styles/Home.module.css";
 
-import { Quicksand } from "next/font/google";
-import Meta from '../components/Meta';
+import Meta from '@/components/Meta';
+import DrawingCanvas from '@/components/DrawingCanvas';
 
-const quicksand = Quicksand({ subsets: ['latin'] })
+import beforeGif from '../../public/assets/2025/bee_my_valentine.gif'
+import afterGif from '../../public/assets/2025/yipee_bee.gif'
+
+import { useState } from "react";
 
 export default function Home() {
+    const [circledYes, setCircledYes] = useState(false);
 
-  return (
-    <>
-      <Meta
-        title="send this to your crush 💌"
-        description="digital valentine's day card"
-        image="social-2024.jpeg"
-      />
-      <main className={`${styles.underConstruction} ${quicksand.className}`}>
-        <div>
-        this page is a work in progress!<br/>
+    const handleCircledYes = () => {
+      setCircledYes(true);
+    }
 
-        in the meanwhile, check out our <a href="archive-2024">archive</a>
+    const renderBefore = () => {
+      return (
+        <div className={styles.container}>
+          <main className={styles.main}>
+              <div className={styles.backgroundContainer}>
+                  <Image
+                      src={beforeGif}
+                      alt="messenger bee coming up to you and asking if you would bee my valentine"
+                      className={styles.backgroundGif}
+                  />
+              </div>
+          </main>
+          <DrawingCanvas
+              onYes={handleCircledYes}
+          />
         </div>
-      </main>
-    </>
+      );
+    }
+
+    const renderAfter = () => {
+      return (
+        <div className={styles.container}>
+          <main className={styles.main}>
+              <div className={styles.backgroundContainer}>
+                  <Image
+                      src={afterGif}
+                      alt="messenger bee coming up to you and asking if you would bee my valentine"
+                      className={styles.backgroundGif}
+                  />
+              </div>
+          </main>
+        </div>
+      );
+    }
+    
+    const renderContent = () => {
+      if (circledYes) {
+        return renderAfter();
+      } else {
+        return renderBefore();
+      }
+    };
+
+    return (
+      <>
+          <Meta
+              title="send this to your crush 💌"
+              description="digital valentine's day card"
+              image="social-2025.jpeg"
+          />
+          {renderContent()}
+      </>
   );
 }
